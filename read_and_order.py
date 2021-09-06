57c95e7cc82fefd264f4e5e53c6eefec6a26821e
Pedro <pedro_rga@hotmail.com>
2021-09-06 10:38:48 -0300

    update wrong save name

--- read_and_order.py
+++ read_and_order.py
@@ -9,7 +9,7 @@ for i in os.listdir("Output/"):
     if i.isdigit():
         i = int(i)
 
-    if os.path.isfile(f"Output/{i}/measures_test.csv"):
+    if os.path.isfile(f"Output/{i}/measures_test.txt"):
         df = pd.read_csv(f"Output/{i}/measures_test.txt", sep=",")
         df.columns = ["HR5", "HR10", "HR20", "NDCG5", "NDCG10", "NDCG20"]
         indexes.append(i)
@@ -21,7 +21,7 @@ order_indexes = np.argsort(metrics)[::-1]
 to_read_order = np.array(indexes)[order_indexes]
 metrics_order = np.array(metrics)[order_indexes]
 
-with open("Output/results.txt", "w") as fo:
+with open("Output/results.csv", "w") as fo:
     parameters_names = "BATCH_SIZE, NEGSAMPLES, DIM, EPOCH_MAX, SEED, LOSSFUN,\
                               num_baseline_dropouts, local_losfun, add_l2_reg_on_risk, add_loss_on_risk, alpha_risk,\
                               do_diff_to_ideal_risk, eval_ideal_risk, dataset, LR, id"
