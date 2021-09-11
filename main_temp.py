c4f877913953b11fecb2a9cdda79f49ed89d530e
Pedro <pedro_rga@hotmail.com>
2021-09-11 10:28:14 -0300

    multi dropout completing and ml1m

--- main_temp.py
+++ main_temp.py
@@ -7,11 +7,11 @@ from tfrankmain_multipledrop_risk_temp import LocalEval
 if __name__ == '__main__':
     # executor = ThreadPoolExecutor(max_workers=2)
     def run():
-        id = 1
+        id = 100
         all_lists = []
         for LOSSFUN in ["neural_sort_cross_entropy_loss", "gumbel_approx_ndcg_loss", "pairwise_logistic_loss", "list_mle_loss"]:
         # for LOSSFUN in ["NeuralSortCrossEntropyLossLocal", "GumbelApproxNDCGLossLocal","PairwiseLogisticLossLocal"]:
-            for num_baseline_dropouts in [1, 3, 5, 10]:
+            for num_baseline_dropouts in [1]:
                 for local_losfun in [""]:
                     for add_l2_reg_on_risk in [True]:
                         for add_loss_on_risk in [True]:
@@ -23,13 +23,13 @@ if __name__ == '__main__':
                                         # for dataset in ["ml100k"]:
                                             for LR in [0.001]:
                                                 drop_rate = 0.1
-                                                if num_baseline_dropouts == 1:
-                                                    drop_rate = 0
+                                                # if num_baseline_dropouts == 1:
+                                                #     drop_rate = 0
 
                                                 list_of_args = [num_baseline_dropouts, local_losfun, add_l2_reg_on_risk,
                                                                 add_loss_on_risk,
                                                                 alpha_risk,
-                                                                do_diff_to_ideal_risk, eval_ideal_risk, dataset, LR, LOSSFUN, drop_rate, id]
+                                                                do_diff_to_ideal_risk, eval_ideal_risk, dataset, LR, LOSSFUN, drop_rate, 20, id]
                                                 all_lists.append(list_of_args)
                                                 id += 1
                                                 # LocalEval(all_lists)
@@ -43,6 +43,51 @@ if __name__ == '__main__':
         results = pool.map(LocalEval, all_lists)
         for r in results:
             print(r)
-    #     # submits.append(a)
-    #
-    #     # if id == 4: return
+
+
+
+    def run2():
+        id = 1001
+        all_lists = []
+        for LOSSFUN in ["neural_sort_cross_entropy_loss", "gumbel_approx_ndcg_loss", "pairwise_logistic_loss", "list_mle_loss"]:
+        # for LOSSFUN in ["NeuralSortCrossEntropyLossLocal", "GumbelApproxNDCGLossLocal","PairwiseLogisticLossLocal"]:
+            for num_baseline_dropouts in [1, 3, 5, 10]:
+                for local_losfun in [""]:
+                    for add_l2_reg_on_risk in [True]:
+                        for add_loss_on_risk in [True]:
+                            for alpha_risk in [2]:
+                                for do_diff_to_ideal_risk in [True]:
+                                    for eval_ideal_risk in [False]:
+                                        for dataset in ["ml1m"]:
+                                            # for dataset in ["ml100k"]:
+                                        # for dataset in ["ml100k"]:
+                                            for LR in [0.0005]:
+                                                # drop_rate = 0.1
+                                                if num_baseline_dropouts == 1:
+                                                    # drop_rate = 0
+
+                                                    list_of_args = [num_baseline_dropouts, local_losfun, add_l2_reg_on_risk,
+                                                                    add_loss_on_risk,
+                                                                    alpha_risk,
+                                                                    do_diff_to_ideal_risk, eval_ideal_risk, dataset, LR, LOSSFUN, 0, 45, id]
+                                                    all_lists.append(list_of_args)
+                                                    id += 1
+
+                                                list_of_args = [num_baseline_dropouts, local_losfun, add_l2_reg_on_risk,
+                                                                add_loss_on_risk,
+                                                                alpha_risk,
+                                                                do_diff_to_ideal_risk, eval_ideal_risk, dataset, LR,
+                                                                LOSSFUN, 0.1, 45, id]
+                                                all_lists.append(list_of_args)
+                                                id += 1
+                                                # LocalEval(all_lists)
+                                                # if id == 1: return all_lists
+        return all_lists
+
+
+    all_lists = run2()
+    with multiprocessing.Pool(processes=2) as pool:
+        # a = executor.submit(LocalEval, list_of_args)
+        results = pool.map(LocalEval, all_lists)
+        for r in results:
+            print(r)
