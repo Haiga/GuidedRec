47bbcfa9ef5020cb095c6a359c4b41d38a68c216
Pedro <pedro_rga@hotmail.com>
2021-09-13 09:36:23 -0300

    add utils to grec

---
+++ grec/move_and_order.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+import numpy as np
+import sys
+
+indexes = []
+metrics = []
+
+new_name = sys.argv[1]
+os.makedirs(f"Output/{new_name}/")
+for i in os.listdir("Output/"):
+    if i.isdigit():
+        os.rename(f"Output/{i}/", f"Output/{new_name}/{i}/")
+
+    if os.path.isfile(f"Output/{i}"):
+        os.rename(f"Output/{i}", f"Output/{new_name}/{i}")
+
