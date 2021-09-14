ad519213a27395b611f257870f78795277f7e7d7
Pedro <pedro_rga@hotmail.com>
2021-09-14 11:42:26 -0300

    update lr

--- grec/mainp2.py
+++ grec/mainp2.py
@@ -7,38 +7,25 @@ if __name__ == '__main__':
     # executor = ThreadPoolExecutor(max_workers=2)
     def run():
         base_args = []
-        id = 200
-        for num_baseline_dropouts in [1, 3, 5, 10]:
-            if num_baseline_dropouts == 1:
-                list_of_args = [0,
-                                num_baseline_dropouts,
-                                "",
-                                True,
-                                "None",
-                                True,
-                                True,
-                                True,
-                                False,
-                                2,
-                                str(id),
-                                0.0001, id]
-                id += 1
-                base_args.append(list_of_args)
-
-            list_of_args = [0.1,
-                            num_baseline_dropouts,
-                            "",
-                            True,
-                            "None",
-                            True,
-                            True,
-                            True,
-                            False,
-                            2,
-                            str(id),
-                            0.0001, id]
-            id += 1
-            base_args.append(list_of_args)
+        id = 300
+        for num_baseline_dropouts in [2, 5]:
+            for alpha_risk in [2, 5]:
+                for local_losfun in ["NeuralSortCrossEntropyLossLocal", "GumbelApproxNDCGLossLocal"]:
+                    for LR_LOGLOSS in [0.0005, 0.0002]:
+                        list_of_args = [0.1,
+                                        num_baseline_dropouts,
+                                        local_losfun,
+                                        True,
+                                        "",
+                                        True,
+                                        True,
+                                        True,
+                                        False,
+                                        alpha_risk,
+                                        str(id),
+                                        LR_LOGLOSS, id]
+                        id += 1
+                        base_args.append(list_of_args)
 
         return base_args
 
