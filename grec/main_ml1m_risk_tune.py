0b2bc0242e7f3174d9bd131b3fe7bd4d108d495a
Pedro <pedro_rga@hotmail.com>
2021-09-16 11:05:32 -0300

    continuing main_best_params for ml1m tune guided rec

---
+++ grec/main_ml1m_risk_tune.py
@@ -0,0 +1,42 @@
+# from concurrent.futures.thread import ThreadPoolExecutor
+import multiprocessing
+
+from guided_rec_parametrized_ml1m import RunGuidedRecParametrized
+
+if __name__ == '__main__':
+    # executor = ThreadPoolExecutor(max_workers=2)
+    def run():
+        base_args = []
+        id = 10000
+
+        for num_baseline_dropouts in [2, 5]:
+            for alpha_risk in [2, 5]:
+                for local_losfun in ["NeuralSortCrossEntropyLossLocal", "GumbelApproxNDCGLossLocal"]:
+                    for LR_LOGLOSS in [0.0005, 0.0001]:
+                        list_of_args = [0.05,
+                                        num_baseline_dropouts,
+                                        local_losfun,
+                                        True,
+                                        "",
+                                        True,
+                                        True,
+                                        True,
+                                        False,
+                                        alpha_risk,
+                                        str(id),
+                                        LR_LOGLOSS, id]
+                        id += 1
+                        base_args.append(list_of_args)
+
+        return base_args
+
+
+    all_lists = run()
+    with multiprocessing.Pool(processes=2) as pool:
+        # a = executor.submit(LocalEval, list_of_args)
+        results = pool.map(RunGuidedRecParametrized, all_lists)
+        for r in results:
+            print(r)
+        # submits.append(a)
+
+        # if id == 4: return
