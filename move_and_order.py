2fc5189079cb5c292bce2f0d2a57b027815e58c1
Pedro <pedro_rga@hotmail.com>
2021-10-04 14:04:16 -0300

    mlm1m tune logloss

--- move_and_order.py
+++ move_and_order.py
@@ -10,7 +10,8 @@ new_name = sys.argv[1]
 os.makedirs(f"Output/{new_name}/")
 for i in os.listdir("Output/"):
     if i.isdigit():
-        if not (990000 <= int(i) <= 999999):
+        # if not (990000 <= int(i) <= 999999):
+        if True:
             os.rename(f"Output/{i}/", f"Output/{new_name}/{i}/")
 
     if os.path.isfile(f"Output/{i}"):
