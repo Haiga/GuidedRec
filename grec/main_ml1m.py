bb4a44f29f5a095143cc736278cb3f627256c457
Pedro <pedro_rga@hotmail.com>
2021-10-05 10:11:10 -0300

    music execution

--- grec/main_ml1m.py
+++ grec/main_ml1m.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     # executor = ThreadPoolExecutor(max_workers=2)
     def run():
         base_args = []
-        id = 1
+        id = 8947563
         for num_baseline_dropouts in [1, 3, 5, 10]:
             if num_baseline_dropouts == 1:
                 list_of_args = [0,
@@ -40,24 +40,24 @@ if __name__ == '__main__':
             id += 1
             base_args.append(list_of_args)
 
-        for num_baseline_dropouts in [2, 5]:
-            for alpha_risk in [2, 5]:
-                for local_losfun in ["NeuralSortCrossEntropyLossLocal", "GumbelApproxNDCGLossLocal"]:
-                    for LR_LOGLOSS in [0.0005, 0.0001]:
-                        list_of_args = [0.1,
-                                        num_baseline_dropouts,
-                                        local_losfun,
-                                        True,
-                                        "",
-                                        True,
-                                        True,
-                                        True,
-                                        False,
-                                        alpha_risk,
-                                        str(id),
-                                        LR_LOGLOSS, id]
-                        id += 1
-                        base_args.append(list_of_args)
+        # for num_baseline_dropouts in [2, 5]:
+        #     for alpha_risk in [2, 5]:
+        #         for local_losfun in ["NeuralSortCrossEntropyLossLocal", "GumbelApproxNDCGLossLocal"]:
+        #             for LR_LOGLOSS in [0.0005, 0.0001]:
+        #                 list_of_args = [0.1,
+        #                                 num_baseline_dropouts,
+        #                                 local_losfun,
+        #                                 True,
+        #                                 "",
+        #                                 True,
+        #                                 True,
+        #                                 True,
+        #                                 False,
+        #                                 alpha_risk,
+        #                                 str(id),
+        #                                 LR_LOGLOSS, id]
+        #                 id += 1
+        #                 base_args.append(list_of_args)
 
         return base_args
 
---
+++ grec/main_ml1m2.py
@@ -0,0 +1,73 @@
+# from concurrent.futures.thread import ThreadPoolExecutor
+import multiprocessing
+
+from guided_rec_parametrized_ml1m import RunGuidedRecParametrized
+
+if __name__ == '__main__':
+    # executor = ThreadPoolExecutor(max_workers=2)
+    def run():
+        base_args = []
+        id = 8967563
+        # for num_baseline_dropouts in [1, 3, 5, 10]:
+        #     if num_baseline_dropouts == 1:
+        #         list_of_args = [0,
+        #                         num_baseline_dropouts,
+        #                         "",
+        #                         True,
+        #                         "None",
+        #                         True,
+        #                         True,
+        #                         True,
+        #                         False,
+        #                         2,
+        #                         str(id),
+        #                         0.0005, id]
+        #         id += 1
+        #         base_args.append(list_of_args)
+        #
+        #     list_of_args = [0.1,
+        #                     num_baseline_dropouts,
+        #                     "",
+        #                     True,
+        #                     "None",
+        #                     True,
+        #                     True,
+        #                     True,
+        #                     False,
+        #                     2,
+        #                     str(id),
+        #                     0.0005, id]
+        #     id += 1
+        #     base_args.append(list_of_args)
+
+        for num_baseline_dropouts in [2, 5]:
+            for alpha_risk in [2, 5]:
+                for local_losfun in ["NeuralSortCrossEntropyLossLocal", "GumbelApproxNDCGLossLocal"]:
+                    for LR_LOGLOSS in [0.0005, 0.0001]:
+                        list_of_args = [0.1,
+                                        num_baseline_dropouts,
+                                        local_losfun,
+                                        True,
+                                        "",
+                                        True,
+                                        True,
+                                        True,
+                                        False,
+                                        alpha_risk,
+                                        str(id),
+                                        LR_LOGLOSS, id]
+                        id += 1
+                        base_args.append(list_of_args)
+
+        return base_args
+
+
+    all_lists = run()
+    with multiprocessing.Pool(processes=3) as pool:
+        # a = executor.submit(LocalEval, list_of_args)
+        results = pool.map(RunGuidedRecParametrized, all_lists)
+        for r in results:
+            print(r)
+        # submits.append(a)
+
+        # if id == 4: return
